f73082355865ebf3fe988d85c1a21fc631759dcd
Pavel <Pavel@A-Profile>
2025-09-08 23:22:09 +0500

    add help command

--- src/handlers/misc.py
+++ src/handlers/misc.py
@@ -1,8 +1,22 @@
 from telebot import types
+from ..config import Settings
 
 def register_handlers(bot):
     @bot.message_handler(commands=['id'])
     def show_id(message: types.Message):
-        print("[DEBUG] Обработчик /id вызван", message.chat.id)
         bot.send_message(message.chat.id, f"chat_id: {message.chat.id}")
+
+    @bot.message_handler(commands=["help"])
+    def on_help(message):
+        managers_chat_id = Settings.managers_chat_id
+        if not managers_chat_id or message.chat.id != managers_chat_id:
+            return
+
+        text = (
+            "📖 Доступные команды для менеджеров:\n\n"
+            "/weekly – заявки в работе за последние 7 дней\n"
+            "/monthly – заявки в работе за последние 30 дней\n"
+            "/help – показать это сообщение\n"
+        )
+        bot.send_message(message.chat.id, text, parse_mode="HTML")
 
